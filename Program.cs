352887cb220578e3969624d5cd6848c60c72e353
elkhateeb <elkhateebelkhairi565@gmail.com>
2026-05-04 18:48:11 +0400

    Add login change

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IAiService, AiService>();
 builder.Services.AddControllers();
 builder.Services.AddHttpClient();
+Console.WriteLine("hello");
+Console.WriteLine("login");
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
